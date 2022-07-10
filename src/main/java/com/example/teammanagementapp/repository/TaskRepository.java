a2c6a37ed9a9041c13ed25f78da9c5e0df21beee
tadeuszpaprocki <tadek.paprocki@gmail.com>
2022-07-10 17:10:17 +0200

    Added TfindByTitle method to TaskRepository

--- src/main/java/com/example/teammanagementapp/repository/TaskRepository.java
+++ src/main/java/com/example/teammanagementapp/repository/TaskRepository.java
@@ -3,8 +3,10 @@ package com.example.teammanagementapp.repository;
 import com.example.teammanagementapp.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 
-
+    Optional<TaskEntity> findByTitle(String title);
 }
