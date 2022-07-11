684b7d0709420768c117174522c0e7150e600908
tadeuszpaprocki <tadek.paprocki@gmail.com>
2022-07-11 13:03:32 +0200

    Added: getSingleTask, getAllTasks, editTask, editTaskPartially, addTask methods to TaskService

---
+++ src/main/java/com/example/teammanagementapp/services/TaskService.java
@@ -0,0 +1,56 @@
+package com.example.teammanagementapp.services;
+
+import com.example.teammanagementapp.dto.TaskDTO;
+import com.example.teammanagementapp.entities.TaskEntity;
+import com.example.teammanagementapp.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    private final ModelMapper modelMapper;
+
+    public Optional<TaskDTO> getSingleTask(String title){
+        return taskRepository.findByTitle(title).map(o -> modelMapper.map(o, TaskDTO.class));
+    }
+
+    public List<TaskDTO> getAllTasks(){
+        return taskRepository.findAll().stream()
+                .map(o -> modelMapper.map(o, TaskDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public Optional<TaskDTO> editTask(TaskDTO taskDTO)
+    {
+        taskRepository.save(modelMapper.map(taskDTO, TaskEntity.class));
+        return Optional.of(taskDTO);
+    }
+
+    public Optional<TaskDTO> editTaskPartially(TaskDTO taskDTO){
+        return taskRepository.findByTitle(taskDTO.getTitle())
+                .map( entity -> {
+                        Optional.of(taskDTO.getTitle()).ifPresent(entity::setTitle);
+                        Optional.of(taskDTO.getDescription()).ifPresent(entity::setDescription);
+                        Optional.of(taskDTO.getStatus()).ifPresent(entity::setStatus);
+                        Optional.of(taskDTO.getDateOfExection()).ifPresent(entity::setDateOfExection);
+                        return Optional.of(modelMapper.map(taskDTO, TaskDTO.class));
+                }).orElseGet(Optional::empty);
+    }
+
+    public boolean addTask(TaskDTO taskDTO){
+        if(taskDTO.getTitle() == taskRepository.findByTitle(taskDTO.getTitle()).get().getTitle()){
+            return false;
+        }
+        taskRepository.save(modelMapper.map(taskDTO, TaskEntity.class));
+        return true;
+    }
+}
