b0220459ab0b88a241e478cadcb4f148ae3a4fba
tadeuszpaprocki <tadek.paprocki@gmail.com>
2022-07-12 09:09:39 +0200

    Fixed method addTask

--- src/main/java/com/example/teammanagementapp/services/TaskService.java
+++ src/main/java/com/example/teammanagementapp/services/TaskService.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -19,38 +20,38 @@ public class TaskService {
 
     private final ModelMapper modelMapper;
 
-    public Optional<TaskDTO> getSingleTask(String title){
+    public Optional<TaskDTO> getSingleTask(String title) {
         return taskRepository.findByTitle(title).map(o -> modelMapper.map(o, TaskDTO.class));
     }
 
-    public List<TaskDTO> getAllTasks(){
+    public List<TaskDTO> getAllTasks() {
         return taskRepository.findAll().stream()
                 .map(o -> modelMapper.map(o, TaskDTO.class))
                 .collect(Collectors.toList());
     }
 
-    public Optional<TaskDTO> editTask(TaskDTO taskDTO)
-    {
+    public Optional<TaskDTO> editTask(TaskDTO taskDTO) {
         taskRepository.save(modelMapper.map(taskDTO, TaskEntity.class));
         return Optional.of(taskDTO);
     }
 
-    public Optional<TaskDTO> editTaskPartially(TaskDTO taskDTO){
+    public Optional<TaskDTO> editTaskPartially(TaskDTO taskDTO) {
         return taskRepository.findByTitle(taskDTO.getTitle())
-                .map( entity -> {
-                        Optional.of(taskDTO.getTitle()).ifPresent(entity::setTitle);
-                        Optional.of(taskDTO.getDescription()).ifPresent(entity::setDescription);
-                        Optional.of(taskDTO.getStatus()).ifPresent(entity::setStatus);
-                        Optional.of(taskDTO.getDateOfExection()).ifPresent(entity::setDateOfExection);
-                        return Optional.of(modelMapper.map(taskDTO, TaskDTO.class));
+                .map(entity -> {
+                    Optional.of(taskDTO.getTitle()).ifPresent(entity::setTitle);
+                    Optional.of(taskDTO.getDescription()).ifPresent(entity::setDescription);
+                    Optional.of(taskDTO.getStatus()).ifPresent(entity::setStatus);
+                    Optional.of(taskDTO.getDateOfExection()).ifPresent(entity::setDateOfExection);
+                    return Optional.of(modelMapper.map(taskDTO, TaskDTO.class));
                 }).orElseGet(Optional::empty);
     }
 
-    public boolean addTask(TaskDTO taskDTO){
-        if(taskDTO.getTitle() == taskRepository.findByTitle(taskDTO.getTitle()).get().getTitle()){
-            return false;
+    public boolean addTask(TaskDTO taskDTO) {
+        if (Objects.equals(taskDTO.getTitle(), taskRepository.findByTitle(taskDTO.getTitle()).get().getTitle())) {
+            taskRepository.save(modelMapper.map(taskDTO, TaskEntity.class));
+            return true;
         }
-        taskRepository.save(modelMapper.map(taskDTO, TaskEntity.class));
-        return true;
+        return false;
+
     }
 }
